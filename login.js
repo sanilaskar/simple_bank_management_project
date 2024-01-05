6b989ad7bb58919bb426aa9451ad1d5db0deec76
shlaskar <shlaskar10@gmail.com>
2024-01-05 00:08:42 -0800

    first commit

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //console.log('clicked work');
+    emailField = document.getElementById('email-field');
+    email = emailField.value;
+   
+    console.log(email);
+    passwordField = document.getElementById('password-field');
+    password = passwordField.value;
+     console.log( password);
+
+})
